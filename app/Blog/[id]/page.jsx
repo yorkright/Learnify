bd1545ddf5f3fd7e0465b025eb3c250a6e214266
Adarsh Narayan Chaubey <156434840+yorkright@users.noreply.github.com>
2025-08-15 12:27:10 +0530

    Update page.jsx.  Blog/[id]. 02

--- app/Blog/[id]/page.jsx
+++ app/Blog/[id]/page.jsx
@@ -45,7 +45,7 @@ const Page = ({ params: paramsPromise }) => {
   }
 
   return (
-    <div className="min-h-screen pt-24 text-black sm:bg-gray-100">
+    <div className="min-h-screen pt-24 text-black sm:text-white sm:bg-gray-100">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         {/* Title */}
         <h1 className="text-4xl md:text-5xl font-bold text-center mt-10 max-w-4xl mx-auto leading-tight mb-6">
